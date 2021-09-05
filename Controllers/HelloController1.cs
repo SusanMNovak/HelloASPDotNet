3a8ea355fe53a50a1d6aae45bbd65404baea79c3
susan.novak@yahoo.com <susan.novak@yahoo.com>
2021-09-05 17:41:19 -0500

    Add project files.

---
+++ Controllers/HelloController1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloASPDotNet.Controllers
+{
+    public class HelloController1 : Controller
+    {
+        //GET: /<controller>/
+        public IActionResult Index()
+        {
+            string html = "<h1>Hello World!<h1>";
+            return Content(html,"text/html");
+        }
+    }
+}
