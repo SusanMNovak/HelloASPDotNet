d71c983b106013d0a7a21f7766656ccf5484813a
susan.novak@yahoo.com <susan.novak@yahoo.com>
2021-09-06 20:35:31 -0500

    No greeting displayed

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -11,8 +11,82 @@ namespace HelloASPDotNet.Controllers
         //GET: /<controller>/
         public IActionResult Index()
         {
-            string html = "<h1>Hello World!<h1>";
-            return Content(html,"text/html");
+            string html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<select name='language'><option value='english' selected>English</option>" +
+                "<option value='spanish'>Spanish</spanish>" +
+                "<option value='bosnian'>Bosnian</option>" +
+                "<option value='vietnamese'>Vietnamese</option>" +
+                "<option value='slovak'>Slovak</option></select>" +
+                "<input type='submit' value='Greet Me!'/>" +
+                "</form>";
+
+            return Content(html, "text/html");
+
         }
+
+        [Route("/Hello")]
+        [HttpPost]
+        public IActionResult Display(string name = "World", string language = "english")
+        {
+            return Content(CreateMessage(name, language));
+
+        }
+
+        public static string CreateMessage(string name, string language)
+        {
+            string helloTranslation = "";
+            /* switch (language)
+             {
+                 case "slovak":
+                     helloTranslation = "Dobry Den ";
+                     break;
+                 case "spanish":
+                     helloTranslation = "Hola ";
+                     break;
+                 case "bosnian":
+                     helloTranslation = "Zdravo ";
+                     break;
+                 case "vietnamese":
+                     helloTranslation = "Xin Chao ";
+                     break;
+                 case "english":
+                     helloTranslation = "Hello ";
+                     break;
+             }*/
+
+            if (language == "slovak")
+            {
+                helloTranslation = "Dobry Den ";
+            }
+
+            else if (language == "spanish")
+            {
+                helloTranslation = "Hola ";
+            }
+
+            else if (language == "bosnian")
+            {
+                helloTranslation = "Zdravo ";
+            }
+
+            else if (language == "vietnamese")
+            {
+                helloTranslation = "Xin Chao ";
+            }
+
+                //Default English
+            else
+            {
+                helloTranslation = "Hello ";
+            }
+
+            return helloTranslation + name;
+
+           
+        }
+
+
     }
 }
+
